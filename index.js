bb2939a1fdcd044b4363105c6751acfb4588a554
Norbert Sana <sananorbert@gmail.com>
2013-03-13 08:59:36 -0400

    kis takaritas

--- index.js
+++
@@ -1,5 +0,0 @@
-var paragraphs = document.getElementsByTagName("p");
-for (var i = 0; i < paragraphs.length; i++) {
-  var paragraph = paragraphs.item(i);
-  paragraph.style.setProperty("color", "white", null);
-}
